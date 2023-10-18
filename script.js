e9e906f8bcd0cbb1a0668788ef621971f32a407f
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-10-18 14:36:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 //your code here
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 let arr1=[];
-let mapobj={};
-for(let i=0; i<touristSpots.length; i++){
-	let word=words(touristSpots[i]);
-	mapobj[word]=touristSpots[i];
-	let arr1.push(word);
+let hashmap={};
+for(let i=0; i<arr.length; i++){
+	let articlestring=getarticlestring(arr[i]);
+	hashmap[articlestring]=arr[i];
+	 arr1.push(articlestring);
 }
 arr1.sort();
 let output=[];
 arr1.forEach((newstr)=>{
-	output.push(mapobj[newstr]);
+	output.push(hashmap[newstr]);
 })
 console.log(output);
 
-function words(word){
+function getarticlestring(word){
 	let a=word.split(" ");
 	let text=a.filter((w)=>{
 	let	wo=w.toLowerCase();
-		if(wo=="the"||wo=="an"||wo=="a"){
+		if(wo==="the"||wo==="an"||wo==="a"){
 			return false;
 		}
 		return true;
