2e9b3778f49328354ce996d73f0109d1514d86d3
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-10-18 14:17:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let arr1=[];
+let mapobj={};
+for(let i=0; i<touristSpots.length; i++){
+	let word=words(touristSpots[i]);
+	mapobj[word]=touristSpots[i];
+	let arr1.push(word);
+}
+arr1.sort();
+let output=[];
+arr1.forEach((newstr)=>{
+	output.push(mapobj[newstr]);
+})
+console.log(output);
 
+function words(word){
+	let a=word.split(" ");
+	let text=a.filter((w)=>{
+	let	wo=w.toLowerCase();
+		if(wo=="the"||wo=="an"||wo=="a"){
+			return false;
+		}
+		return true;
+	})
+	return text.join("");
+}
